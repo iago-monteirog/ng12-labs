f63996f56c12d113ec65244787febd0b181eeadd
iago-monteirog <iagomgarcia9000@gmail.com>
2024-09-16 15:37:24 -0300

    add layout component and reactive form for login form

--- AppInvestimentos/src/app/app.module.ts
+++ AppInvestimentos/src/app/app.module.ts
@@ -7,17 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastrModule } from 'ngx-toastr';
 import { LoginComponent } from './security/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AppInvestimentos/src/app/security/login/login.component.ts
+++ AppInvestimentos/src/app/security/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formLogin: FormGroup;
+
+  constructor(private fb: FormBuilder, private toast: ToastrService) {
+    this.formLogin = this.criarFormLogin();
+   }
 
   ngOnInit(): void {
+  }
+
+  public criarFormLogin(): FormGroup {
+    return this.fb.group({
+      username: ["",[Validators.required, Validators.minLength(6)]],
+      password: ["",[Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  public isFormControlInvalid(controlName: string): boolean {
+    return !!(this.formLogin.get(controlName)?.invalid && this.formLogin.get(controlName)?.touched)
   }
 
 }
